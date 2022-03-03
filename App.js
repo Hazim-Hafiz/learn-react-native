b0196055e6c55e864c07dc00e29423d340564263
Abdalla Hisham <hazim@snsoerp.com>
2022-03-03 10:02:21 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Hazim')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
+      <Text></Text>
+      <View>
+        <Button title="change value" onPress={() => setName('another name')}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +23,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  circle: {
+    marginTop: -40,
+    height: 80, 
+    width: 200,
+    borderRadius: 50,
+    backgroundColor: 'pink'
+  },  
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+    borderRadius: 15,
+    height: 300,
+    width: 300,
+    marginTop: -10
+  },
+  bold: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 18,
+  },
+  body: {
+    height: 200,
+    width: 250,
+    backgroundColor: '#f2f2f2',
+    fontWeight: 'bold',
+    fontSize: 18,
+    borderRadius: 15,
+    paddingHorizontal: 20,
+  },
+  textbody: {
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 5,
+    color: 'black'
+  },
+  poll: {
+    marginTop: -10,
+    backgroundColor: 'pink',
+    height: 200,
+    width: 20,
+    borderRadius: 15,
+  }
 });
